a3a8bbc423f47eb9f854e9b5513d52d5a81017b5
harithasnair <haritha.s@zoho.com>
2019-05-29 23:13:26 +0530

    checking db changes in UI

--- ANAGRAM_Sqlite/app/src/main/java/com/harithasnair/anagram_sqlite/DatabaseHelperClass.java
+++ ANAGRAM_Sqlite/app/src/main/java/com/harithasnair/anagram_sqlite/DatabaseHelperClass.java
@@ -3,29 +3,74 @@ package com.harithasnair.anagram_sqlite;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.os.Build;
+import android.util.Log;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.file.Path;
 
 public class DatabaseHelperClass extends SQLiteOpenHelper {
 
-    private static String DB_PATH = "";
-    private static String DB_NAME = "ANAGRAM.db";
+    private  String DB_PATH = "";
+    private static String DB_NAME = "word.db";
     private SQLiteDatabase mDatabase;
-    private Context mContext = null;
+    private final Context mContext;
 
     public DatabaseHelperClass(Context context) {
         super(context, DB_NAME, null, 1);
-        if (Build.VERSION.SDK_INT >= 22)
-            DB_PATH = mContext.getApplicationInfo().dataDir + "/databases/";
-        else
-            DB_PATH = "/data/data/" + mContext.getPackageName() + "/databases/";
-        mContext = context;
+        this.mContext = context;
+        this.DB_PATH = (mContext.getPackageName() +"/assets/databases/");
+        Log.e("path 1", DB_PATH);
+    }
+
+    public void createDataBase() throws IOException {
+        boolean DBExist = checkDataBase();
+        if (DBExist) {
+        } else {
+            this.getReadableDatabase();
+            try {
+                copyDataBase();
+            } catch (IOException e) {
+                throw new Error("Error copying database");
+            }
+        }
+    }
+
+
+    public boolean checkDataBase() {
+        SQLiteDatabase tempDB = null;
+        try {
+            String path = DB_PATH+DB_NAME;
+            tempDB = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READONLY);
+        } catch (SQLiteException e) {
+        }
+        if (tempDB != null) {
+            tempDB.close();
+        }
+        return tempDB != null ? true : false;
+    }
+
+    private void copyDataBase() throws IOException {
+        InputStream myInput = mContext.getAssets().open("word.db");
+        String outputFileName = DB_PATH+DB_NAME;
+        OutputStream myOutput = new FileOutputStream(outputFileName);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
+
+    public void openDataBase() {
+        String path = DB_PATH+DB_NAME;
+        mDatabase = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READONLY);
     }
 
     public synchronized void close() {
@@ -34,65 +79,25 @@ public class DatabaseHelperClass extends SQLiteOpenHelper {
         super.close();
     }
 
-    public boolean checkDataBase() {
-        SQLiteDatabase tempDB = null;
-        try {
-            String path = DB_PATH + DB_NAME;
-            tempDB = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READWRITE);
-        } catch (Exception e)
-        if (tempDB != null)
-            tempDB.close();
-        return tempDB != null ? true : false;
-    }
-
-
-    public void copyDataBase() {
-        try {
-            InputStream myInput = mContext.getAssets().open(DB_NAME);
-            String outputFileName = DB_PATH + DB_NAME;
-            OutputStream myOutput = new FileOutputStream(outputFileName);
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = myInput.read(buffer)) > 0) {
-                myOutput.write(buffer, 0, length);
-            }
-            myOutput.flush();
-            myOutput.close();
-            myInput.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void openDataBase() {
-        String path = DB_PATH + DB_NAME;
-        mDatabase = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READWRITE);
-    }
-
-    public void createDataBase() {
-        boolean isDBExist = checkDataBase();
-        if (isDBExist) {
-        } else
-            this.getReadableDatabase();
-        try {
-            copyDataBase();
-        } catch (Exception e) {
-
-        }
-    }
-
-    SQLiteDatabase database = this.getWritableDatabase();
-              String projection[] = {"WORD"};
-         Cursor c = database.query("WORD_TABLE",projection,null,null,null,null,null);
-
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (newVersion > oldVersion)
+            try {
+                copyDataBase();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
 
+    public Cursor query(String table, String[] columns, String selection, String[] selectionArgs, String groupby, String having, String orderBy) {
+        return mDatabase.query("table", null, null, null, null, null, null);
     }
 }
+
+
+
--- ANAGRAM_Sqlite/app/src/main/java/com/harithasnair/anagram_sqlite/MainActivity.java
+++ ANAGRAM_Sqlite/app/src/main/java/com/harithasnair/anagram_sqlite/MainActivity.java
@@ -1,21 +1,24 @@
 package com.harithasnair.anagram_sqlite;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.Toast;
+
+import java.io.IOException;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView anagram;
-    private Button nextButton;
+    private Button play;
 
     DatabaseHelperClass mydb;
+    Cursor c = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,68 +26,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         anagram = (TextView) (findViewById(R.id.tv_01));
-        nextButton = (Button) findViewById(R.id.play_button);
-//
-//        mydb = new DatabaseHelperClass(this);
+        play = (Button) findViewById(R.id.play_button);
 
-        // insert data
-//        SQLiteDatabase database = mydb.getWritableDatabase();
-//        String projection[] = {"ToBeSolvedWords"};
-//        Cursor c = database.query("wordsTable",projection,null,null,null,null,null);
-//
-//        ContentValues values = new ContentValues();
+        play.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                DatabaseHelperClass dbHelper = new DatabaseHelperClass(MainActivity.this);
+                try {
+                    dbHelper.createDataBase();
+                } catch (IOException ioe) {
+                    throw new Error("Unable to create database");
+                }
 
-        // Implementing HashMap
+                try {
+                    dbHelper.openDataBase();
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
 
-////        values.put("ToBeSolvedWords", "CAMERA");
-//        values.put("ToBeSolvedWords","BRISK");
-//          values.put("ToBeSolvedWords", "POTENT");
-//        values.put("ToBeSolvedWords","CHEESY");
-//        values.put("ToBeSolvedWords", "NINTH");
-//        values.put("ToBeSolvedWords","BLEND");
-//        values.put("ToBeSolvedWords", "AVOID");
-//        values.put("ToBeSolvedWords","GASSY");
-//        values.put("ToBeSolvedWords", "KHAKI");
-//        values.put("ToBeSolvedWords","BANANA");
-//        values.put("ToBeSolvedWords", "STOMP");
-//        values.put("ToBeSolvedWords","CHILD");
-//        values.put("ToBeSolvedWords", "DENOTE");
-//        values.put("ToBeSolvedWords","TALLER");
-//        values.put("ToBeSolvedWords", "FANCY");
-//        values.put("ToBeSolvedWords","TUGGED");
-//        values.put("ToBeSolvedWords", "EMBARK");
-//        values.put("ToBeSolvedWords","REMOVE");
-//        values.put("ToBeSolvedWords", "BLOOM");
-//        values.put("ToBeSolvedWords","WAFER");
-//        long row =  database.insert("wordsTable ","null",values);
-//          System.out.println("row number :" +row);
-
-
-//        SQLiteDatabase database = mydb.getReadableDatabase();
-
-
-//        Cursor c = database.query("wordsTable", projection, null, null, null, null, null);
-
-//         System.out.println("word is :" +c.getString(1));
-//
-
-//        ArrayList<String> data = new ArrayList<>();
-//        if (c.moveToFirst()) {
-//            data.add(c.getString(c.getColumnIndex(COL_2)));
-//            while (c.moveToNext()) {
-//                data.add(c.getString(c.getColumnIndex(COL_2)));
-//            }
-//        }
-//
-//        System.out.println("list of words is :" + data);
-//        anagram.setText(data.get(0));
-//
-//         System.out.println("list of words is :" + anagram);
-//
-//
-//////        c.close();
-//////      database.close();
+                Toast.makeText(MainActivity.this, "Succesfully Imported", Toast.LENGTH_SHORT).show();
+                c = dbHelper.query("table", null, null, null, null, null, null);
+                if (c.moveToFirst()) {
+                    do {
+                        Toast.makeText(MainActivity.this, "WORD:" + c.getString(1), Toast.LENGTH_LONG).show();
+                    } while (c.moveToNext());
+                }
+            }
+        });
     }
 }
-
-
